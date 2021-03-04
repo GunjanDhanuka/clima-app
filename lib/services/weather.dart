d5cb90aefa0a78e1b35fdfd65361e7050951fbd6
Gunjan Dhanuka <lucifergd192@gmail.com>
2021-03-04 12:58:08 +0530

    Finished App

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter/cupertino.dart';
+
+const apiKey = '94f75d7498a6658d70617288f97e9686';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -28,6 +54,24 @@ class WeatherModel {
       return 'You\'ll need 🧣 and 🧤';
     } else {
       return 'Bring a 🧥 just in case';
+    }
+  }
+
+  String getBackgroundImage({int condition}) {
+    if (condition < 300) {
+      return 'thunder.jpg';
+    } else if (condition < 600) {
+      return 'rain.jpg';
+    } else if (condition < 700) {
+      return 'winter.jpg';
+    } else if (condition < 800) {
+      return 'mist.jpg';
+    } else if (condition == 800) {
+      return 'summer.jpg';
+    } else if (condition <= 804) {
+      return 'clouds.jpg';
+    } else {
+      return 'location_background.jpg🤷‍';
     }
   }
 }
